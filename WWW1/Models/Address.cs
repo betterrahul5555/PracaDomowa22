2f650646fdf13a86ff05f0d56163ef890aeb43f5
betterrahul5555 <63734203+betterrahul5555@users.noreply.github.com>
2021-03-14 22:20:43 +0100

    Praca domowa

---
+++ WWW1/Models/Address.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WWW1.Models
+{
+    public class Address
+    {
+        [Display(Name = "Twoja ulubiona ulica")]
+
+       
+        [Required(ErrorMessage = "Pole jest obowiązkowe")]
+        public string Street { get; set; }
+        
+        public string ZipCode { get; set; }
+       
+        
+        public string City { get; set; }
+        
+      
+ [Required(ErrorMessage = "Zły numer")]
+        public int Number { get; set; }
+         
+    }
+}
---
+++ WWW1/Pages/Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using WWW1.Models;
+
+namespace WWW1.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        [BindProperty]
+
+        public Address Address { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string Name { get; set; }
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+ public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            return RedirectToPage("./Privacy");
+        }
+        public void OnGet()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                Name = "User";
+            }
+        }
+       
+
+
+    }
+}
